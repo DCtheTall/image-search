26f000ccfb53ad54141cc713b13e66751274371f
Dylan Cutler <DCtheTall@users.noreply.github.com>
2017-05-27 02:55:17 -0400

    Sets up view

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const morgan = require('morgan');
 
 const app = express();
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 app.use(bodyParser.json({ limit: '5mb' }));
 
 app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
+
+app.get('/', (req, res) => res.render('index'));
 
 module.exports = app;
